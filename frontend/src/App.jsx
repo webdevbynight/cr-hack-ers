be653f1f525f3b356baa4a4619f1a13cfd0eff5f
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-05-11 20:43:15 +0200

    fix scroll to top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Header from "./components/Header";
 import "./styles.scss";
 import Welcome from "./pages/Welcome";
 import Ocean from "./pages/Ocean";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <BrowserRouter>
+  <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Welcome />} />
