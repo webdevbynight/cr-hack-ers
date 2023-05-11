bb12a9f5eada657e3cfa153584a558df68e6349b
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-05-11 13:58:27 +0200

    Welcome page created

---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+function Welcome() {
+  return (
+    <div className="parallax parallax1">
+      <h1>Ocean Adventures</h1>;
+    </div>
+    // <div className="parallax parallax2">
+
+    // </div>
+  );
+}
+
+export default Welcome;
