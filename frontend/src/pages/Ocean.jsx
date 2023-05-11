a5fc93ad67a31bbcb0c3037f44987385144bf736
webdevbynight <contact@victor-brito.name>
2023-05-11 20:36:52 +0200

    Removed props from Ocean component

--- frontend/src/pages/Ocean.jsx
+++ frontend/src/pages/Ocean.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import Activities from "../components/Activities";
 import Locations from "../components/Locations";
 import Informations from "../components/ocean/Informations";
 
 function Ocean() {
-  const { id } = useParams();
   return (
     <>
       <div className="page-bg" />
       <div className="container-page">
         <div className="infos">
-          <Informations id={id} />
+          <Informations />
         </div>
         <div className="locations">
           <Locations />
