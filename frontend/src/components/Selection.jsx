a47b52ec305bef477de2cb8e2980a5caa48190af
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-05-11 18:59:39 +0200

    responsive on welcome

--- frontend/src/components/Selection.jsx
+++ frontend/src/components/Selection.jsx
@@ -1,31 +1,48 @@
+import { useEffect, useState } from "react";
 import Bubbles from "./Bubbles";
 
 function Selection() {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <span className="bubble-container1">
-        <Bubbles />
-      </span>
+      {scrollTop >= 5 && (
+        <span className="bubble-container1">
+          <Bubbles />
+        </span>
+      )}
+      <div className="verybig-container">
+        <div className="verybig-bubbles bubble1">
+          <h3>Océan Pacifique</h3>
+        </div>
+        <div className="verybig-bubbles bubble2">
+          <h3>Océan Arctique</h3>
+        </div>
 
-      <div className="verybig-bubbles bubble1">
-        <h3>Océan Pacifique</h3>
-      </div>
-      <div className="verybig-bubbles bubble2">
-        <h3>Océan Arctique</h3>
-      </div>
+        <div className="verybig-bubbles bubble3">
+          <h3>Océan Atlantique</h3>
+        </div>
 
-      <div className="verybig-bubbles bubble3">
-        <h3>Océan Atlantique</h3>
-      </div>
+        <div className="verybig-bubbles bubble4">
+          <h3>
+            Océan <br /> Indien
+          </h3>
+        </div>
 
-      <div className="verybig-bubbles bubble4">
-        <h3>
-          Océan <br /> Indien
-        </h3>
-      </div>
-
-      <div className="verybig-bubbles bubble5">
-        <h3>Océan Antarctique</h3>
+        <div className="verybig-bubbles bubble5">
+          <h3>Océan Antarctique</h3>
+        </div>
       </div>
     </>
   );
