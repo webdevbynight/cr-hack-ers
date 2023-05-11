49eb31ba8d7c599b19ecea472d0c2d88e09f1816
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-05-11 21:34:23 +0200

    update basket files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,68 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./styles.scss";
 import Welcome from "./pages/Welcome";
 import Basket from "./pages/Basket";
+import test from "./assets/pexels-jeremy-bishop-2397651.jpg";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      title: "salut",
+      img: test,
+      price: 34,
+    },
+    {
+      id: 2,
+      title: "robert",
+      img: test,
+      price: 54,
+    },
+  ];
+  const [carts, setCarts] = useState([]);
+
+  const handleAddItem = (clickedItem) => {
+    setCarts((prev) => {
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      return [...prev, { ...clickedItem, quantity: 1 }];
+    });
+  };
+
+  const handleRemoveItem = (clickedItem) => {
+    setCarts((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === clickedItem.id) {
+          if (item.quantity === 1) return ack;
+          return [...ack, { ...item, quantity: item.quantity - 1 }];
+        }
+        return [...ack, item];
+      }, [])
+    );
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/basket" element={<Basket />} />
+        <Route
+          path="/basket"
+          element={
+            <Basket
+              carts={carts}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              products={products}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,8 +1,54 @@
-function Basket() {
+/* eslint-disable react/prop-types */
+/* eslint-disable no-return-assign */
+import { useEffect, useState } from "react";
+import Button from "../components/ButtonCart";
+
+function Basket({ handleAddItem, handleRemoveItem, carts }) {
+  const [totalItemsQuantity, setTotalItemsQuantity] = useState(0);
+  const [totalItemsPrice, setTotalItemsPrice] = useState(0);
+  useEffect(() => {
+    if (carts) {
+      const totalQuantity = carts.reduce((acc, item) => {
+        return acc + item.quantity;
+      }, 0);
+      const totalPrice = carts.reduce((acc, item) => {
+        return item.price;
+      }, 0);
+      setTotalItemsQuantity(totalQuantity);
+      setTotalItemsPrice(totalPrice);
+    }
+  }, [carts, totalItemsQuantity]);
+  console.info(carts);
   return (
-    <div className="basket-container">
-      <p>Hello</p>
-    </div>
+    <article className="cart-container">
+      {carts.map((cart) => (
+        <div className="cart-box" key={cart.id}>
+          <div className="cart-img">
+            <img src={cart.img} alt="" />
+            <p>{cart.title}</p>
+          </div>
+          <div className="buttons-container">
+            <Button
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              product={cart}
+            />
+            <button className="button-quantity" type="button">
+              {cart.quantity}
+            </button>
+          </div>
+          <div className="button-remove-container">
+            <span>{cart.price * cart.quantity}</span>
+          </div>
+        </div>
+      ))}
+      <div className="total">
+        <span className="text-price">
+          {totalItemsQuantity * totalItemsPrice}
+        </span>
+        <span className="price-value">price €</span>
+      </div>
+    </article>
   );
 }
 
