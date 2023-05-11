f8c4db3df28e399ecca7bb76122ff96d663db05c
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-05-11 17:38:51 +0200

    first visuel of component activities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import "./styles.scss";
-import Locations from "@components/Locations";
-// import Welcome from "./pages/Welcome";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
