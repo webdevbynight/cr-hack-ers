8565ba4549942bfb56b9e150dbc9f0d488f0ee6c
Anne LOUIS <anne.louis51100@gmail.com>
2023-05-11 17:51:47 +0200

    Add ocean page and css and informations.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles.scss";
 import Welcome from "./pages/Welcome";
+import Ocean from "./pages/Ocean";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route to="/" element={<Welcome />} />
+        <Route path="/ocean" element={<Ocean />} />
       </Routes>
     </BrowserRouter>
   );
