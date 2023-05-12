2a57764baa2e7293759eeec28ce0983cc0eb481e
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-05-12 13:07:00 +0200

    added redirection select

--- frontend/src/pages/Ocean.jsx
+++ frontend/src/pages/Ocean.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { PropTypes } from "prop-types";
+import Select from "@components/Select";
 import oceans from "../data/oceans.json";
 import Activities from "../components/Activities";
 import Locations from "../components/Locations";
@@ -14,6 +15,7 @@ function Ocean({ handleAddItem, handleRemoveItem, carts, articles }) {
   return (
     <div className="bg-page">
       <div className="container-page">
+        <Select />
         <h1>{oceanData.title}</h1>
         <div className="infos">
           <Informations />
