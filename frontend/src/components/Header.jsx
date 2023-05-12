8d3970c73c3e9e396aaf8eae9a9906cb430ad37f
Anne LOUIS <anne.louis51100@gmail.com>
2023-05-12 07:14:19 +0200

    Add link Basket to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import canneAPeche from "../assets/canne_a_peche.png";
+
 function Header() {
   return (
     <header className="header">
       <h1 className="title-website">Ocean Adventure</h1>
+      <Link to="/basket">
+        <img
+          className="logo_header"
+          src={canneAPeche}
+          alt="logo canne a peche"
+        />
+      </Link>
     </header>
   );
 }
